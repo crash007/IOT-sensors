65f11efe3c88980376104507ab4f430ade8be9d8
crash007 <wallin.petter@gmail.com>
2015-09-08 14:20:16 +0200

    Default to latest day on charts.

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -3,17 +3,20 @@
 $( document ).ready(function() {
 	console.log(timeSpan);
 	console.log(this.location);
-	$('a[href="' + this.location.pathname + '"]').addClass('active');
-	loadGraphs();
-	updateGraphs();
+	if(typeof timeSpan !=='undefined' && timeSpan!==''){
+		$('a[href="' + this.location.pathname + '"]').addClass('active');
+		loadGraphs();
+		//updateGraphs();
+	}else{
+		window.location.href = this.location.pathname+'/latest/day';
+	}
 
 	
 });
 
 function loadGraphs(){	
 	$.getJSON( '/sensors/json/'+timeSpan, function( data ) {
-        $.each(data, function(index,item){
-        	//console.log(item._id);        
+        $.each(data, function(index,item){     
         	$('.chart-container').append('<div class="panel panel-default sensor-panel"><div class="panel-body"><div id="'+item._id+'"></div></div></div>');
         	chart(item);           
         });        
@@ -22,13 +25,13 @@ function loadGraphs(){
 
 
 // Use a named immediately-invoked function expression.
-function updateGraphs() {
-	$.getJSON( '/sensors/json/'+timeSpan, function( data ) {
-        $.each(data, function(index,item){
-            var chart = $('#'+item._id).highcharts();
-            chart.series[0].setData(timeValueArray(item.data));          
-        });    
-        setTimeout(updateGraphs, 2000);
-    });
-	
-}	
+//function updateGraphs() {
+//	$.getJSON( '/sensors/json/'+timeSpan, function( data ) {
+//        $.each(data, function(index,item){
+//            var chart = $('#'+item._id).highcharts();
+//            chart.series[0].setData(timeValueArray(item.data));          
+//        });    
+//       // setTimeout(updateGraphs, 2000);
+//    });
+//	
+//}	
